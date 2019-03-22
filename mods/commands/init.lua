4d0038125061b757150cfd82d43a9c4618d46a45
AiTechEye <40591179+AiTechEye@users.noreply.github.com>
2019-03-22 09:22:26 +0100

    +home

---
+++ mods/commands/init.lua
@@ -0,0 +1,34 @@
+minetest.register_privilege("home", {
+	description = "Can set/go home",
+	give_to_singleplayer = true,
+})
+
+minetest.register_chatcommand("sethome", {
+	params = "",
+	description = "Set home position",
+	privs = {home=true},
+	func = function(name, param)
+		local player=minetest.get_player_by_name(name)
+		if player then
+			local meta = player:get_meta()
+			local pos = player:get_pos()
+			meta:set_string("home",minetest.pos_to_string(pos))
+			minetest.chat_send_player(name, "Home set at " .. math.floor(pos.x) .." " .. math.floor(pos.y) .." " .. math.floor(pos.z))
+		end
+	end
+})
+
+minetest.register_chatcommand("home", {
+	params = "",
+	description = "Go home",
+	privs = {home=true},
+	func = function(name, param)
+		local player=minetest.get_player_by_name(name)
+		if player then
+			local meta = player:get_meta()
+			local pos = minetest.string_to_pos(meta:get_string("home"))
+			player:set_pos(pos)
+			minetest.chat_send_player(name, "Teleported to home")
+		end
+	end
+})
