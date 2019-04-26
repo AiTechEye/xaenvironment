048886c0f47dc60d8e4d8d7ddaa258aa8af661eb
AiTechEye <40591179+AiTechEye@users.noreply.github.com>
2019-04-26 13:58:18 +0200

    Add files via upload

---
+++ mods/clock/init.lua
@@ -0,0 +1,62 @@
+clock={}
+
+minetest.register_craft({
+	output="clock:clock1",
+	recipe={
+		{"group:stick","default:iron_ingot","group:stick"},
+		{"group:wood","default:paper","group:wood"},
+		{"group:stick","group:wood","group:stick"},
+	},
+})
+
+clock.time=function(pos,s)
+	local t = math.floor((minetest.get_timeofday() * 24)+0.5)
+	if t > 12 then
+		t = t - 12
+	elseif t == 0 then
+		t = 12
+	end
+	if t ~= s then
+		local p =minetest.get_node(pos).param2
+		minetest.swap_node(pos,{name="clock:clock" .. t,param2=p})
+	end
+end
+
+for i=1,12 do
+minetest.register_node("clock:clock" .. i, {
+	description = "Clock",
+	tiles={"clock_"..i ..".png","default_wood.png","default_wood.png","default_wood.png","default_wood.png","default_wood.png",},
+	drop = "clock:clock1",
+	wield_image="clock_1.png",
+	inventory_image="clock_"..i ..".png",
+	groups = {dig_immediate=3,flammable=2,not_in_creative_inventory = i ~= 1 and 1 or nil},
+	sounds = default.node_sound_wood_defaults(),
+	drawtype="nodebox",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, -0.1875, -0.4375, -0.4375, 0.1875},
+			{-0.4375, -0.5, -0.3125, -0.375, -0.4375, 0.3125},
+			{-0.375, -0.5, -0.375, -0.3125, -0.4375, 0.375},
+			{-0.3125, -0.5, -0.4375, -0.1875, -0.4375, 0.4375},
+			{-0.1875, -0.5, -0.5, 0.1875, -0.4375, 0.5},
+			{0.1875, -0.5, -0.4375, 0.3125, -0.4375, 0.4375},
+			{0.3125, -0.5, -0.375, 0.375, -0.4375, 0.375},
+			{0.375, -0.5, -0.3125, 0.4375, -0.4375, 0.3125},
+			{0.4375, -0.5, -0.1875, 0.5, -0.4375, 0.1875},
+		}
+	},
+	selection_box={type="fixed",fixed={-0.5, -0.5, -0.5, 0.5, -0.4375, 0.5}},
+	paramtype2="wallmounted",
+	paramtype = "light",
+	sunlight_propagates = true,
+	after_place_node = function(pos, placer, itemstack)
+		minetest.rotate_node(itemstack,placer,{under=pos,above=pos})
+		minetest.get_node_timer(pos):start(1)
+	end,
+	on_timer = function (pos, elapsed)
+		clock.time(pos,i)
+		return true
+	end,
+})
+end
