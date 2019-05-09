2e5f8cdfcdb804d13c80909fac7781f074f34853
AiTechEye <40591179+AiTechEye@users.noreply.github.com>
2019-05-09 18:15:21 +0200

    Add files via upload

--- mods/plants/init.lua
+++ mods/plants/init.lua
@@ -386,7 +386,8 @@ default.register_plant({
 	end,
 	groups={spreading_plant=20},
 	visual_scale=2.5,
-	selection_box ={type="fixed",fixed={-0.25,-0.5,-0.25,0.25,2,0.25}}
+	selection_box ={type="fixed",fixed={-0.25,-0.5,-0.25,0.25,2,0.25}},
+	dye_colors = {palette=100,hex="#bbffbb",name="dark green"},
 })
 default.register_plant({
 	name="cow_parsnip_big",
@@ -406,7 +407,8 @@ default.register_plant({
 	end,
 	groups={spreading_plant=16},
 	visual_scale=3.5,
-	selection_box ={type="fixed",fixed={-0.25,-0.5,-0.25,0.25,3,0.25}}
+	selection_box ={type="fixed",fixed={-0.25,-0.5,-0.25,0.25,3,0.25}},
+	dye_colors = {palette=100,hex="#bbffbb",name="dark green"},
 })
 
 
@@ -421,7 +423,8 @@ default.register_plant({
 	}},
 	groups={spreading_plant=15},
 	visual_scale=1.3,
-	selection_box ={type="fixed",fixed={-0.25,-0.5,-0.25,0.25,0.8,0.25}}
+	selection_box ={type="fixed",fixed={-0.25,-0.5,-0.25,0.25,0.8,0.25}},
+	dye_colors = {palette=100,hex="#bbffbb",name="dark green"},
 })
 
 default.register_plant({
@@ -435,6 +438,7 @@ default.register_plant({
 	}},
 	groups={spreading_plant=10},
 	visual_scale=1.1,
+	dye_colors = {palette=100,hex="#bbffbb",name="dark green"},
 })
 
 default.register_plant({
