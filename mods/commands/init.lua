7175474ea095e14a1f2fd3bd2a82766f93c78278
AiTechEye <40591179+AiTechEye@users.noreply.github.com>
2019-07-14 10:01:24 +0200

    Add files via upload

--- mods/commands/init.lua
+++ mods/commands/init.lua
@@ -1,3 +1,36 @@
+minetest.register_chatcommand("killme", {
+	params = "",
+	description = "Die",
+	privs = {home=true},
+	func = function(name, param)
+		local p = minetest.get_player_by_name(name)
+		if p then
+			p:set_hp(0)
+		end
+	end
+})
+
+minetest.register_chatcommand("kill", {
+	params = "<player>",
+	description = "Kill",
+	privs = {ban=true},
+	func = function(name, param)
+		local p = minetest.get_player_by_name(param)
+		if p then
+			p:set_hp(0)
+		end
+	end
+})
+
+minetest.register_chatcommand("revokeme", {
+	params = "<priv> or <all>",
+	description = "Revoke privs from yourself",
+	privs = {privs=true},
+	func = function(name, param)
+		minetest.registered_chatcommands["revoke"].func(name,name.." "..param)
+	end
+})
+
 minetest.register_privilege("home", {
 	description = "Can set/go home",
 	give_to_singleplayer = true,
