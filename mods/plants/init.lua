6483ce535c663ac3572f2c761f365eafbf0fcce5
AiTechEye <40591179+AiTechEye@users.noreply.github.com>
2019-07-20 20:53:02 +0200

    Add files via upload

--- mods/plants/init.lua
+++ mods/plants/init.lua
@@ -754,7 +754,7 @@ default.register_plant({
 	on_timer = i < 3 and function(pos, elapsed)
 		if minetest.get_item_group(minetest.get_node({x=pos.x,y=pos.y-1,z=pos.z}).name,"wet_soil") == 0 then
 			minetest.set_node(pos,{name="plants:dry_plant"})
-		elseif default.date("m",minetest.get_meta(pos):get_int("date")) >= 30 and (minetest.get_node_light(pos) or 0) >= 13 then
+		elseif default.date("m",minetest.get_meta(pos):get_int("date")) >= 30 and (minetest.get_node_light(pos,0.5) or 0) >= 13 then
 			minetest.set_node(pos,{name="plants:wheat"..i+1})
 			if i < 3 then
 				minetest.get_meta(pos):set_int("date",default.date("get"))
@@ -794,7 +794,7 @@ minetest.register_node("plants:wheat_seed", {
 		if minetest.get_item_group(minetest.get_node({x=pos.x,y=pos.y-1,z=pos.z}).name,"wet_soil") == 0 then
 			minetest.remove_node(pos)
 			minetest.add_item(pos,"plants:wheat_seed")
-		elseif default.date("m",minetest.get_meta(pos):get_int("date")) >= 1 and (minetest.get_node_light(pos) or 0) >= 13 then
+		elseif default.date("m",minetest.get_meta(pos):get_int("date")) >= 1 and (minetest.get_node_light(pos,0.5) or 0) >= 13 then
 			minetest.set_node(pos,{name="plants:wheat1"})
 			minetest.get_meta(pos):set_int("date",default.date("get"))
 			minetest.get_node_timer(pos):start(1)
