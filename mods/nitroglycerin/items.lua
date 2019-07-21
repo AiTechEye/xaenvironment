fcd5e30613499030b89d2ec6fc07334b2368a1ca
AiTechEye <40591179+AiTechEye@users.noreply.github.com>
2019-07-21 08:48:51 +0200

    Add files via upload

--- mods/nitroglycerin/items.lua
+++ mods/nitroglycerin/items.lua
@@ -6,10 +6,10 @@ minetest.register_node("nitroglycerin:timed_bomb", {
 	on_blast=function(pos)
 		minetest.set_node(pos,{name="air"})
 		minetest.after(0.1, function(pos)
-		for i, ob in pairs(minetest.get_objects_inside_radius(pos, 5)) do
-			local en=ob:get_luaentity()
-			ob:punch(ob,1,{full_punch_interval=1,damage_groups={fleshy=250}})
-		end
+			for i, ob in pairs(minetest.get_objects_inside_radius(pos, 5)) do
+				local en=ob:get_luaentity()
+				ob:punch(ob,1,{full_punch_interval=1,damage_groups={fleshy=250}})
+			end
 		nitroglycerin.explode(pos,{radius=5,set="air"})
 		end,pos)
 	end,
@@ -40,9 +40,9 @@ minetest.register_node("nitroglycerin:timed_bomb", {
 minetest.register_craft({
 	output = "nitroglycerin:timed_bomb 3",
 	recipe = {
-		{"default:steel_ingot","default:electric_lump","default:steel_ingot"},
-		{"default:steel_ingot","default:uraniumactive_ingot","default:steel_ingot"},
-		{"default:steel_ingot","default:steel_ingot","default:steel_ingot"},
+		{"default:iron_ingot","default:electric_lump","default:iron_ingot"},
+		{"default:iron_ingot","default:uraniumactive_ingot","default:iron_ingot"},
+		{"default:iron_ingot","default:iron_ingot","default:iron_ingot"},
 	}
 })
 
@@ -58,15 +58,31 @@ minetest.override_item("default:coalblock",{
 })
 
 minetest.register_craft({
-	output = "nitroglycerin:timed_bomb 3",
+	output = "nitroglycerin:timed_bomb 1",
 	recipe = {
-		{"nitroglycerin:c4","nitroglycerin:c4","nitroglycerin:c4"},
-		{"nitroglycerin:c4","nitroglycerin:c4","nitroglycerin:c4"},
 		{"nitroglycerin:c4","nitroglycerin:c4","nitroglycerin:c4"},
 	}
 })
 
 minetest.register_craftitem("nitroglycerin:c4", {
 	description = "C4",
-	inventory_image = "nitroglycerin_c4.png"
+	inventory_image = "nitroglycerin_c4.png",
+	on_use=function(itemstack, user, pointed_thing)
+		if pointed_thing.type == "object" then
+			local ref = pointed_thing.ref
+			minetest.after(5, function(ref)
+				local pos = ref:get_pos()
+				if pos then
+					for i, ob in pairs(minetest.get_objects_inside_radius(pos, 2)) do
+						local en=ob:get_luaentity()
+						ob:punch(ob,1,{full_punch_interval=1,damage_groups={fleshy=50}})
+					end
+					nitroglycerin.explode(pos,{radius=2,set="air"})
+				end
+			end,ref)
+			itemstack:take_item()
+			minetest.sound_play("nitroglycerin_activated", {pos=pointed_thing.ref:get_pos(), gain = 1, max_hear_distance = 7})
+			return itemstack
+		end
+	end
 })
