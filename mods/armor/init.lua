5f7d3c1a8238af1629e20d3a5202baebd5e36967
AiTechEye <40591179+AiTechEye@users.noreply.github.com>
2019-07-23 23:18:59 +0200

    Add files via upload

--- mods/armor/init.lua
+++ mods/armor/init.lua
@@ -1,24 +1,22 @@
 if 1 then return end
 
--- this is just a test, its not really working yet
-
+not ready yet
 
 armor={
-	bones=minetest.settings:get_bool("xaenvironment_itemlosing") ==  true,
-	types={chestplate=1,gloves=2,boots=3,leggings=4,overall=5},
+	types={chestplate=1,helmet=2,gloves=3,boots=4,leggings=5,overall=6},
 	user={},
-	playerlevel=100,
+	registered_items={}
 }
 
 player_style.register_button({
 	name="armor",
-	image="armor:chestplate",
+	image="armor:iron_chestplate",
 	type="item_image",
 	info="Armor",
 	action=function(user)
 		local name = user:get_player_name()
 		minetest.after(0.2, function(name)
-		return minetest.show_formspec(name, "exaachievements",
+		return minetest.show_formspec(name, "armor",
 			"size[8,6]" ..
 			"listcolors[#77777777;#777777aa;#000000ff]"..
 			"list[detached:armor;main;2.5,0;3,2;]" ..
@@ -32,7 +30,8 @@ player_style.register_button({
 
 armor.update=function(player,wear)
 	local name = player:get_player_name()
-	local inv = armor.user[name]:get_list("main")
+	local data = armor.user[name]
+	local inv = data.inv:get_list("main")
 	local d = {}
 	local arml = 0
 	wear = wear or 0
@@ -43,55 +42,110 @@ armor.update=function(player,wear)
 	end
 	player:get_meta():set_string("armor",minetest.serialize(d))
 
-	if player:get_armor_groups() ~= armor.playerlevel+arml then
-		player:set_armor_groups({flashy=armor.playerlevel+arml})
+	data.playerlevel = data.playerlevel or player:get_armor_groups().fleshy
+
+	if player:get_armor_groups() ~= armor.user[name].playerlevel+arml then
+		player:set_armor_groups({fleshy=data.playerlevel+arml})
+		if not data.skin then
+			data.skin = player:get_properties().textures[1]
+		end
+		local texture = data.skin
+		local t
+		for i,v in pairs(inv) do
+			
+			if v:get_name() ~= "" then
+				t = true
+				texture = texture .. "^"..armor.registered_items[v:get_name()]
+			end
+		end
+		if t then
+			player:set_properties({textures={texture}})
+		end
 	end
 end
 
 armor.register_item=function(name,def)
 	if type(def.type) ~= "string" then
-		error('Armor: declare a specific "type" : chestplate, gloves, boots, leggings, overall')
+		error('Armor: declare a specific "type" : chestplate, helmet, gloves, boots, leggings, overall')
 	elseif not armor.types[def.type] then
-		error("Armor: invaild type ("..def.type..") : chestplate, gloves, boots, leggings, overall")
+		error("Armor: invaild type ("..def.type..") : chestplate, helmet, gloves, boots, leggings, overall")
 	elseif not def.image then
 		error("Armor: declare a image")
 	end
 	local mod = minetest.get_current_modname() ..":"
+	local texture = "([combine:64x32:"
 
-	minetest.register_tool(mod..name, {
-		description=def.description or string.gsub(name,"_"," "),
-		wield_image="[combine:16x16:0,0:"..def.image.."^armor_alpha_chestplate.png^[makealpha:0,255,0",
+	for i=0,64,16 do
+		texture =  texture ..":0,"..i.."="..def.image ..":16,"..i.."="..def.image
+	end
+
+	armor.registered_items[mod..name.."_"..def.type] = texture .. "^armor_alpha_"..def.type..".png^[makealpha:0,255,0)"
+
+	minetest.register_tool(mod..name.."_"..def.type, {
+		description=def.description or name.upper(string.sub(name,1,1)) .. string.sub(name,2,string.len(name)).." "..def.type,
+		inventory_image=def.image.."^armor_alpha_"..def.type.."_item.png^[makealpha:0,255,0",
 		groups = {level=def.level or 1,armor=armor.types[def.type],weaknes=math.abs(math.floor(65000/(def.resistance or 100)))},
 	})
 
-	if def.item and def.type == "chestplate" then
-		minetest.register_craft({
-			output=mod..name,
-			recipe={
-				{def.item,"",def.item},
-				{def.item,def.item,def.item},
-				{def.item,def.item,def.item},
-			}
-		})
+	if def.item then
+		local recipe = {
+			chestplate={{def.item,"",def.item},{def.item,def.item,def.item},{def.item,def.item,def.item}},
+			helmet={{def.item,def.item,def.item},{def.item,"",def.item}},
+			leggings={{def.item,def.item,def.item},{def.item,"",def.item},{def.item,"",def.item}},
+			boots={{def.item,"",def.item},{def.item,"",def.item}},
+			gloves={{def.item,"",def.item}},
+			overall={{def.item,def.item,def.item},{def.item,def.item,def.item},{def.item,"",def.item}},
+
+		}
+		minetest.register_craft({output=mod..name.."_"..def.type,recipe=recipe[def.type]})
 	end
 end
 
 dofile(minetest.get_modpath("armor") .. "/items.lua")
 
+table.insert(bones.functions_add,function(player,inv)
+	local name = player:get_player_name()
+	local ainv = armor.user[name].inv:get_list("main")
+	for i,v in pairs(ainv) do
+		inv:add_item("main",v)
+	end
+end)
+
+table.insert(bones.functions_drop,function(player,pos)
+	local name = player:get_player_name()
+	local ainv = armor.user[name].inv:get_list("main")
+	for i,v in pairs(ainv) do
+		minetest.add_item(pos,v)
+	end
+end)
+
+table.insert(bones.functions_remove,function(player)
+	local ainv=armor.user[player:get_player_name()].inv
+	for i,v in pairs(ainv:get_list("main")) do
+		ainv:remove_item("main",v)
+	end
+	armor.update(player)
+end)
+
 minetest.register_on_punchplayer(function(player, hitter, time_from_last_punch, tool_capabilities, dir, damage)
 	armor.update(player,tool_capabilities.damage_groups.fleshy or 1)
 end)
---[[
+
 minetest.register_on_player_hpchange(function(player,hp_change,modifer)
 	if player then
 		armor.update(player,hp_change)
 	end
 	return hp_change
 end,true)
---]]
-minetest.register_on_joinplayer(function(user)
-	local name = user:get_player_name()
-	armor.user[name]=minetest.create_detached_inventory("armor", {
+
+minetest.register_on_leaveplayer(function(player)
+	armor.user[player:get_player_name()] = nil
+end)
+
+minetest.register_on_joinplayer(function(player)
+	local name = player:get_player_name()
+	armor.user[name]={}
+	armor.user[name].inv=minetest.create_detached_inventory("armor", {
 		allow_move = function(inv, from_list, from_index, to_list, to_index, count, player)
 			return count
 		end,
@@ -104,35 +158,15 @@ minetest.register_on_joinplayer(function(user)
 		on_put = function(inv, listname, index, stack, player)
 			armor.update(player)
 		end,
+		on_take = function(inv, listname, index, stack, player)
+			armor.update(player)
+		end,
 	})
-	armor.user[name]:set_size("main", 6)
+	armor.user[name].inv:set_size("main", 6)
 	local list = {}
-	for i,v in pairs(minetest.deserialize(user:get_meta():get_string("armor") or "") or {}) do
+	for i,v in pairs(minetest.deserialize(player:get_meta():get_string("armor") or "") or {}) do
 		table.insert(list,ItemStack(v))
 	end
-	armor.user[name]:set_list("main", list)
-end)
-
-table.insert(bones.functions_add,function(player,inv)
-	local name = player:get_player_name()
-	local ainv = armor.user[name]:get_list("main")
-	for i,v in pairs(ainv) do
-		inv:add_item("main",v)
-	end
-end)
-
-table.insert(bones.functions_drop,function(player,pos)
-	local name = player:get_player_name()
-	local ainv = armor.user[name]:get_list("main")
-	for i,v in pairs(ainv) do
-		minetest.add_item(pos,v)
-	end
-end)
-
-table.insert(bones.functions_remove,function(player)
-	local ainv=armor.user[player:get_player_name()]
-	for i,v in pairs(ainv:get_list("main")) do
-		ainv:remove_item("main",v)
-	end
+	armor.user[name].inv:set_list("main", list)
 	armor.update(player)
 end)
