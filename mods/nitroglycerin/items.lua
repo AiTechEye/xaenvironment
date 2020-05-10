f5f2f8fb0a4a9c73e129378cc8f37a4bbe3200e6
AiTechEye <40591179+AiTechEye@users.noreply.github.com>
2020-05-10 21:53:49 +0200

    Add files via upload

--- mods/nitroglycerin/items.lua
+++ mods/nitroglycerin/items.lua
@@ -7,7 +7,6 @@ minetest.register_node("nitroglycerin:timed_bomb", {
 		minetest.set_node(pos,{name="air"})
 		minetest.after(0.1, function(pos)
 			for i, ob in pairs(minetest.get_objects_inside_radius(pos, 5)) do
-				local en=ob:get_luaentity()
 				ob:punch(ob,1,{full_punch_interval=1,damage_groups={fleshy=250}})
 			end
 		nitroglycerin.explode(pos,{radius=5,set="air"})
@@ -38,12 +37,58 @@ minetest.register_node("nitroglycerin:timed_bomb", {
 	end,
 })
 
+minetest.register_node("nitroglycerin:timed_nuclear_bomb", {
+	description = "Timed nuclear bomb",
+	tiles = {"nitroglycerin_timed_bomb.png^[colorize:#aaff0055"},
+	groups = {dig_immediate = 2,mesecon = 2,flammable = 5,treasure=2,exatec_wire_connected=1},
+	sounds = default.node_sound_wood_defaults(),
+	on_blast=function(pos)
+		minetest.set_node(pos,{name="air"})
+		minetest.after(0.1, function(pos)
+			for i, ob in pairs(minetest.get_objects_inside_radius(pos, 80)) do
+				ob:punch(ob,1,{full_punch_interval=1,damage_groups={fleshy=2000}})
+			end
+			nitroglycerin.explode(pos,{radius=40,set="air",blow_nodes=0})
+		end,pos)
+	end,
+	on_timer=function(pos, elapsed)
+		minetest.registered_nodes["nitroglycerin:timed_nuclear_bomb"].on_blast(pos)
+	end,
+	on_rightclick = function(pos, node, player, itemstack, pointed_thing)
+		local meta=minetest.get_meta(pos)
+		if meta:get_int("b")==1 then return end
+		meta:set_int("b",1)
+		minetest.get_node_timer(pos):start(5)
+		minetest.sound_play("nitroglycerin_activated", {pos=pos, gain = 1, max_hear_distance = 7})
+	end,
+
+	exatec={
+		on_wire = function(pos)
+			minetest.sound_play("nitroglycerin_activated", {pos=pos, gain = 1, max_hear_distance = 7})
+			minetest.registered_nodes["nitroglycerin:timed_nuclear_bomb"].on_blast(pos)
+		end
+	},
+	on_burn = function(pos)
+		minetest.registered_nodes["nitroglycerin:timed_nuclear_bomb"].on_rightclick(pos)
+	end,
+	on_ignite = function(pos, igniter)
+		minetest.registered_nodes["nitroglycerin:timed_nuclear_bomb"].on_rightclick(pos)
+	end,
+})
+
 minetest.register_craft({
 	output = "nitroglycerin:timed_bomb 3",
 	recipe = {
 		{"default:iron_ingot","default:electric_lump","default:iron_ingot"},
 		{"default:iron_ingot","default:uraniumactive_ingot","default:iron_ingot"},
 		{"default:iron_ingot","default:iron_ingot","default:iron_ingot"},
+	}
+})
+
+minetest.register_craft({
+	output = "nitroglycerin:timed_nuclear_bomb",
+	recipe = {
+		{"default:uraniumactive_ingot","nitroglycerin:timed_bomb","default:uraniumactive_ingot"},
 	}
 })
 
@@ -56,13 +101,6 @@ minetest.override_item("default:coalblock",{
 		minetest.remove_node(pos)
 		nitroglycerin.explode(pos,{radius=2,set="air"})
 	end
-})
-
-minetest.register_craft({
-	output = "nitroglycerin:timed_bomb 1",
-	recipe = {
-		{"nitroglycerin:c4","nitroglycerin:c4","nitroglycerin:c4"},
-	}
 })
 
 minetest.register_craftitem("nitroglycerin:c4", {
@@ -87,4 +125,11 @@ minetest.register_craftitem("nitroglycerin:c4", {
 			return itemstack
 		end
 	end
+})
+
+minetest.register_craft({
+	output = "nitroglycerin:c4",
+	recipe = {
+		{"default:uranium_lump","default:iron_ingot"},
+	}
 })
