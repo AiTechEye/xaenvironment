23ab0b6fe300239443164b25794853e5fd2cccd9
AiTechEye <40591179+AiTechEye@users.noreply.github.com>
2020-06-16 13:32:47 +0200

    Add files via upload

--- mods/default/nodes.lua
+++ mods/default/nodes.lua
@@ -477,7 +477,7 @@ minetest.register_node("default:stone", {
 	sounds = default.node_sound_stone_defaults(),
 })
 
-minetest.register_node("default:stone", {
+minetest.register_node("default:space_stone", {
 	description = "Space stone",
 	tiles={"default_space_stone.png"},
 	groups = {stone=1,cracky=3,treasure=1},
