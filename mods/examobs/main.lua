849b242327c48a8f6b03d1849ba90677cf5c7f31
AiTechEye <40591179+AiTechEye@users.noreply.github.com>
2020-06-16 13:00:50 +0200

    Add files via upload

--- mods/examobs/main.lua
+++ mods/examobs/main.lua
@@ -72,6 +72,8 @@ examobs.register_mob=function(def)
 	def.spawn_chance =		def.spawn_chance or		100
 	def.spawn_on =			def.spawn_on or			{"group:spreading_dirt_type","group:sand","default:snow"}
 	def.spawn_in =			def.spawn_in
+	def.max_spawn_y =		def.max_spawn_y or		500
+	def.min_spawn_y =			def.min_spawn_y or		31000
 	def.light_min =			def.light_min or			9
 	def.light_max =			def.light_max or			15
 	def.lifetime =			def.lifetime or			300
@@ -338,7 +340,7 @@ examobs.register_mob=function(def)
 			local pos1 = apos(pos,0,1)
 			local pos2 = apos(pos,0,2)
 			local l=minetest.get_node_light(pos1)
-			if examobs.global_lifetime <= 1 and l and math.random(1,def.spawn_chance) == 1 and l >= def.light_min and l <= def.light_max then
+			if pos1.y >= def.min_spawn_y and pos2.y <= def.max_spawn_y and examobs.global_lifetime <= 1 and l and math.random(1,def.spawn_chance) == 1 and l >= def.light_min and l <= def.light_max then
 				local n1 = minetest.get_node(pos1).name
 				if (def.spawn_in and (def.spawn_in==n1 and def.spawn_in==minetest.get_node(pos2).name or minetest.get_item_group(n1,def.spawn_in) > 0))  or not (walkable(pos1) and walkable(pos2)) then 
 					minetest.add_entity(apos(pos1,0,bottom), name):set_yaw(math.random(0,6.28))
