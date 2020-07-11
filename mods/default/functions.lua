dc84be937f8bb1499b908d17ec2329c1ecea4193
AiTechEye <40591179+AiTechEye@users.noreply.github.com>
2020-07-11 12:22:38 +0200

    Add files via upload

--- mods/default/functions.lua
+++ mods/default/functions.lua
@@ -740,7 +740,9 @@ default.treasure=function(def)
 	else
 		items = def.items
 	end
-	if def.node ~= "" then
+	if type(def.node) == "table" then
+		minetest.set_node(def.pos,def.node)
+	else
 		minetest.set_node(def.pos,{name=def.node})
 	end
 	local m = minetest.get_meta(def.pos):get_inventory()
