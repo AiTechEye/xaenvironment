f47c88eb8e5025b539d81b461bc1fb9e9478c2d8
AiTechEye <40591179+AiTechEye@users.noreply.github.com>
2020-10-22 11:48:00 +0200

    Add files via upload

--- mods/player_style/special.lua
+++ mods/player_style/special.lua
@@ -1,16 +1,38 @@
 special={
 	user={},
+	num = 5,
 	blocks = {
-		"default:qblock_FF0000",
-		"default:qblock_1c7800",
-		"default:qblock_e29f00",
-		"default:qblock_800080",
-		"default:qblock_0000FF",
-	},
-	abilities = {
-
+		["default:qblock_FF0000"]={i=1},
+		["default:qblock_1c7800"]={i=2},
+		["default:qblock_e29f00"]={i=3,ability="Fire resistance",
+			trigger=function(player)
+				local m = player:get_meta()
+				m:set_int("fire_resistance",m:get_int("fire_resistance")+100)
+			end,
+			use=function(player)
+				local m = player:get_meta()
+				local f  = m:get_int("fire_resistance")
+				if f > 0 then
+					m:set_int("fire_resistance",f-1)
+					return true
+				end
+			end,
+			count = function(player)
+				return player:get_meta():get_int("fire_resistance")
+			end
+		},
+		["default:qblock_800080"]={i=4},
+		["default:qblock_0000FF"]={i=5},
 	}
 }
+
+special.use_ability=function(player,ab)
+	for i,v in pairs(special.blocks) do
+		if v.ability == ab then
+			return v.use(player)
+		end
+	end
+end
 
 player_style.register_button({
 	name="special",
@@ -23,30 +45,41 @@ player_style.register_button({
 })
 
 special.show=function(player)
-	local name = player:get_player_name()
-	minetest.after(0.2, function(name)
+	minetest.after(0.2, function(player)
+		local name = player:get_player_name()
 		local inv = special.user[name].inv
 		local slots = ""
-		local x = 1.5
 		for i,v in pairs(special.blocks) do
-			slots = slots .. "item_image["..x..",0;1,1;"..v.."]"
-			if inv:get_stack("main",i):get_count() > 0 then
-				if special.abilities[i] then
-					slots = slots .. "image_button["..x..",1;1,1;player_style_coin.png;specialbut_"..i..";100]"
+			slots = slots .. "item_image["..(v.i+0.5)..",0.2;1,1;"..i.."]"
+			if inv:get_stack("main",v.i):get_count() > 0 then
+				if v.trigger then
+					slots = slots .. "label["..(v.i+0.5)..",-0.3;"..v.count(player).."]" ..
+					"image_button["..(v.i+0.5)..",1.2;1,1;player_style_coin.png;specialbut_"..i..";100]"
 				else
-					slots = slots .. "label["..x..",1;unable]"
+					slots = slots .. "label["..(v.i+0.5)..",1;yet\nunable]"
 				end
 			end
-			x = x +1
 		end
 		return minetest.show_formspec(name, "special",
 		"size[8,6]" ..
 		"listcolors[#77777777;#777777aa;#000000ff]"..
-		"list[detached:special;main;1.5,0;"..#special.blocks..",1;]" ..
+		"list[detached:special;main;1.5,0.2;"..special.num..",1;]" ..
 		"list[current_player;main;0,2.3;8,4;]" ..
 		slots)
-	end, name)
+	end, player)
 end
+
+minetest.register_on_player_receive_fields(function(player, form, pressed)
+	if form == "special" then
+		for i,v in pairs(pressed) do
+			if string.sub(i,1,11) == "specialbut_" then
+				special.blocks[string.sub(i,12,-1)].trigger(player)
+				special.show(player)
+				return
+			end
+		end
+	end
+end)
 
 special.update=function(player)
 	local name = player:get_player_name()
@@ -70,7 +103,8 @@ minetest.register_on_joinplayer(function(player)
 			return 0
 		end,
 		allow_put = function(inv, listname, index, stack, player)
-			return special.blocks[index] == stack:get_name() and inv:get_stack("main",index):get_count() == 0 and 1 or 0
+			local b = special.blocks[stack:get_name()]
+			return b and b.i == index and inv:get_stack("main",b.i):get_count() == 0 and 1 or 0
 		end,
 		allow_take = function(inv, listname, index, stack, player)
 			return stack:get_count()
@@ -84,10 +118,10 @@ minetest.register_on_joinplayer(function(player)
 			special.show(player)
 		end,
 	})
-	special.user[name].inv:set_size("main", #special.blocks)
+	special.user[name].inv:set_size("main", special.num)
 	local list = {}
 	for i,v in pairs(minetest.deserialize(player:get_meta():get_string("special") or "") or {}) do
-		list[i] = v.name--ItemStack(v)
+		list[i] = v.name
 	end
 	special.user[name].inv:set_list("main", list)
 	special.update(player)
