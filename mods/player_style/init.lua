f77bc642d66400e584f78c99deb61dfb2799093c
AiTechEye <40591179+AiTechEye@users.noreply.github.com>
2020-10-30 14:29:37 +0100

    Add files via upload

--- mods/player_style/init.lua
+++ mods/player_style/init.lua
@@ -760,11 +760,7 @@ player_style.player_diveing=function(name,player,a,water,kong)
 		if player:get_player_velocity().y < 0 then
 			if minetest.get_item_group(nod,"water") > 0 then
 				local d = default.def(nod)
-				if d.drawtype and d.drawtype == "flowingliquid" then
-					minetest.sound_play("default_item_watersplash", {object=player, gain = 4,max_hear_distance = 10})
-				else
-					minetest.sound_play("default_object_watersplash", {object=player, gain = 4,max_hear_distance = 10})
-				end
+				default.watersplash(pos,d.drawtype and d.drawtype == "flowingliquid")
 			elseif minetest.get_item_group(nod,"lava") > 0 then
 				minetest.sound_play("default_clay_step", {object=player, gain = 4,max_hear_distance = 10})
 			end
