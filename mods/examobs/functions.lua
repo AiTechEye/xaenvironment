ae51433f06aa032976be013aa1d9f7637f8e6d87
AiTechEye <40591179+AiTechEye@users.noreply.github.com>
2020-10-30 14:32:07 +0100

    Add files via upload

--- mods/examobs/functions.lua
+++ mods/examobs/functions.lua
@@ -294,7 +294,7 @@ examobs.environment=function(self)
 	if def.liquid_viscosity > 0 then
 		if not self.in_liquid and v.y <= 0 then
 			if minetest.get_item_group(def.name,"water") > 0 then
-				minetest.sound_play("default_object_watersplash", {object=self.object, gain = 4,max_hear_distance = 10})
+				default.watersplash(pos)
 			elseif minetest.get_item_group(def.name,"lava") > 0 then
 				minetest.sound_play("default_clay_step", {object=self.object, gain = 4,max_hear_distance = 10})
 			end
