b9fb3a7d368e88bf19d89decfcb41c569cf5ef85
AiTechEye <40591179+AiTechEye@users.noreply.github.com>
2020-12-06 14:56:23 +0100

    Add files via upload

--- mods/paintnings/init.lua
+++ mods/paintnings/init.lua
@@ -70,3 +70,67 @@ minetest.register_craft({
 		{"default:dye","default:stick","default:dye"},
 	},
 })
+
+minetest.register_craft({
+	output="paintnings:tv_off",
+	recipe={
+		{"default:dye","materials:iron_chest","default:lamp"},
+		{"materials:plastic_sheet","materials:diode","materials:plastic_sheet"},
+		{"materials:plastic_sheet","default:glass_tabletop","materials:plastic_sheet"},
+	},
+})
+minetest.register_node("paintnings:tv_off", {
+	description = "TV (off)",
+	tiles={"default_ironblock.png^[colorize:#000b","default_ironblock.png^[colorize:#000b","default_ironblock.png^[colorize:#000b","default_ironblock.png^[colorize:#000b","default_ironblock.png^[colorize:#000b","default_stone.png^[colorize:#000f"},
+	groups = {choppy = 2, oddly_breakable_by_hand = 2,flammable=3},
+	paramtype2 = "facedir",
+	on_punch=function(pos, node, player, pointed_thing)
+		minetest.set_node(pos,{name="paintnings:tv_1",param2=node.param2})
+	end,
+	on_construct=function(pos)
+		minetest.get_meta(pos):set_string("infotext","Punch toggle on/off\nClick change channel")
+	end
+})
+
+for i,v in pairs({
+	{t="paintnings_tv_space",w=124,h=107,sound="plasma_core_loaded",loop=1.98},
+	{t="paintnings_tv_news",w=80,h=72,sound="examobs_titan_growl",loop=10,s=2},
+	{t="paintnings_tv_dig",w=72,h=72,sound="default_stone_dig",loop=0.26},
+	{t="paintnings_tv_icecreammonster",w=94,h=84,sound="examobs_heavy_step",loop=0.32},
+	{t="paintnings_tv_ocean",w=124,h=112,sound="default_underwater",loop=5.8},
+	{t="paintnings_tv_vexcazer",w=124,h=112,s=2,sound="vexcazer_lazer",loop=5},
+	{t="paintnings_tv_rain",w=124,h=112,s=2,sound="weather_rain",loop=5},
+}) do
+minetest.register_node("paintnings:tv_"..i, {
+	description = "TV",
+	drop="paintnings:tv_off",
+	groups = {choppy = 2, oddly_breakable_by_hand = 2,flammable=3,not_in_creative_inventory=1},
+	paramtype2 = "facedir",
+	light_source = 13,
+	tiles={"default_ironblock.png^[colorize:#000b","default_ironblock.png^[colorize:#000b","default_ironblock.png^[colorize:#000b","default_ironblock.png^[colorize:#000b","default_ironblock.png^[colorize:#000b",
+		{
+			name = v.t..".png",
+			backface_culling = false,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = v.w,
+				aspect_h = v.h,
+				length = v.s or 1,
+			}
+		}
+	},
+	on_punch=function(pos, node, player, pointed_thing)
+		minetest.set_node(pos,{name="paintnings:tv_off",param2=node.param2})
+	end,
+	on_rightclick=function(pos, node, player, itemstack, pointed_thing)
+		minetest.set_node(pos,{name="paintnings:tv_"..(i < 7 and i+1 or 1),param2=node.param2})
+	end,
+	on_construct=function(pos)
+		minetest.get_node_timer(pos):start(v.loop)
+	end,
+	on_timer = function(pos, elapsed)
+		minetest.sound_play(v.sound, {pos = pos,gain = 5,max_hear_distance = 10})
+		return true
+	end
+})
+end
