3a610720a533b8df16c45eb84a2310f6f41d9274
AiTechEye <40591179+AiTechEye@users.noreply.github.com>
2020-12-19 12:16:12 +0100

    Add files via upload

--- mods/exacarts/init.lua
+++ mods/exacarts/init.lua
@@ -178,7 +178,7 @@ exacarts.register_rail({-- gives weird acceleration
 	description="Direction rail",
 	inventory_image="(default_ironblock.png^[combine:16x16:0,0=exacarts_rails_alpha.png^[makealpha:0,255,0)^default_crafting_arrowup.png",
 	tiles = {"(default_ironblock.png^[combine:16x16:0,0=exacarts_rails_alpha.png^[makealpha:0,255,0)^default_crafting_arrowup.png"},
-	add_groups = {exatec_wire=1,exatec_wire_connected=1,store=200},
+	add_groups = {exatec_wire_connected=1,store=200},
 	paramtype2 = "facedir",
 	drawtype = "nodebox",
 	node_box={
