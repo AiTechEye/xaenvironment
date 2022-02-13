b64907cec0bdc7caac47e1c1eacfc1ef23fcb488
AiTechEye <40591179+AiTechEye@users.noreply.github.com>
2022-02-13 19:07:51 +0100

    Add files via upload

---
+++ mods/deprecated/init.lua
@@ -0,0 +1,28 @@
+deprecated = {
+	{
+		replace = {"examobs:","materials:"},
+		items = {
+			"examobs:sugar",
+			"examobs:sugar_with_glaze",
+			"examobs:sponge_cake",
+			"examobs:marzipan_rose",
+			"examobs:candy1",
+			"examobs:candy2",
+			"examobs:candy3",
+			"examobs:candy4",
+			"examobs:candy5",
+			"examobs:candy6",
+			"examobs:gel",
+			"examobs:gel_flowing",
+			"examobs:gel2",
+			"examobs:gel_flowing2"
+		}
+	}
+}
+
+for i1,v2 in pairs(deprecated) do
+	for i,v in pairs(v2.items) do
+		print(v:gsub(v2.replace[1],v2.replace[2]),v)
+		minetest.register_alias(v,v:gsub(v2.replace[1],v2.replace[2]))
+	end
+end
