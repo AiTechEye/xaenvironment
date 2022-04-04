6dbfd533bfb57cff3aaf554debc9e307f969e27c
AiTechEye <40591179+AiTechEye@users.noreply.github.com>
2022-04-04 20:17:23 +0200

    Add files via upload

--- mods/places/init.lua
+++ mods/places/init.lua
@@ -1,5 +1,15 @@
 places = {
 	buildings = {
+		["cloud castle"]={
+			chance=100,sx=112,sy=16,miny=240,maxy=260,spawn_at={"air"},
+			on_spawn=function(pos)
+				minetest.place_schematic(pos,minetest.get_modpath("places").."/schematics/places_cloudcastle.mts","random",nil,true,"place_center_x,place_center_z")
+				local nodes = minetest.find_nodes_in_area(vector.subtract(pos,50),vector.add(pos,50),"examobs:cloud_chest")
+				for i,v in pairs(nodes) do
+					default.treasure({level=2,pos=v,node="examobs:cloud_chest"})
+				end
+			end
+		},
 		["stairs to sky"]={
 			chance=50,sx=10,sy=250,miny=0,maxy=50,spawn_at={"default:dirt"},
 			on_spawn=function(pos)
@@ -87,21 +97,13 @@ places = {
 	}
 }
 
-minetest.register_tool("places:spawn", {
-	description = "Spawn place",
-	inventory_image = "default_stick.png",
-	on_use=function(itemstack, user, pointed_thing)
-		local pos = user:get_pos()
-		places.buildings["stairs to sky"].on_spawn(vector.round(pos))
-		--minetest.place_schematic(pos,minetest.get_modpath("villages").."/schematics/atlantis_building"..i..".mts","random",nil,true,"place_center_x,place_center_z")
-	end
-})
-
 minetest.register_on_generated(function(minp, maxp, seed)
 	local vm,min,max
 	local area
 	local data
 	for i,v in pairs(places.buildings) do
+local r = math.random(0,v.chance)
+print(i," ",r)
 		if math.random(0,v.chance) == 0 and minp.y-100 <= v.miny and maxp.y+100 >= v.maxy then
 			local spawn_at = {}
 			for i1,v1 in pairs(v.spawn_at) do
@@ -115,6 +117,8 @@ minetest.register_on_generated(function(minp, maxp, seed)
 			for t=0,10 do
 				local tx = vector.new(math.random(minp.x,maxp.x),maxp.y,math.random(minp.z,maxp.z))
 				for y=maxp.y,minp.y,-1 do
+
+print(y," ", v.miny ," ", v.maxy," ",spawn_at[data[area:index(tx.x,y,tx.z)]])
 					if y >= v.miny and y <= v.maxy and spawn_at[data[area:index(tx.x,y,tx.z)]] then
 						local sp = vector.new(tx.x,y,tx.z)
 						minetest.emerge_area(vector.subtract(sp,v.sx,v.sy,v.sx),vector.add(sp,v.sx,v.sy,v.sx))
@@ -128,3 +132,12 @@ minetest.register_on_generated(function(minp, maxp, seed)
 		end
 	end
 end)
+
+minetest.register_tool("places:spawn", {
+	description = "Spawn place",
+	inventory_image = "default_stick.png",
+	on_use=function(itemstack, user, pointed_thing)
+		local pos = user:get_pos()
+		places.buildings["stairs to sky"].on_spawn(vector.round(pos))
+	end
+})
