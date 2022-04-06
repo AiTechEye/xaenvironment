c07d728f1fe697ffaf49820c5a6255466b8af4e1
AiTechEye <40591179+AiTechEye@users.noreply.github.com>
2022-04-06 19:09:43 +0200

    Add files via upload

--- mods/places/init.lua
+++ mods/places/init.lua
@@ -1,7 +1,7 @@
 places = {
 	buildings = {
 		["cloud castle"]={
-			chance=100,sx=112,sy=16,miny=240,maxy=260,spawn_at={"air"},
+			chance=10,sx=112,sy=16,miny=200,maxy=300,spawn_at={"air"},
 			on_spawn=function(pos)
 				minetest.place_schematic(pos,minetest.get_modpath("places").."/schematics/places_cloudcastle.mts","random",nil,true,"place_center_x,place_center_z")
 				local nodes = minetest.find_nodes_in_area(vector.subtract(pos,50),vector.add(pos,50),"examobs:cloud_chest")
@@ -10,8 +10,67 @@ places = {
 				end
 			end
 		},
+		["lava castle"]={
+			chance=200,sx=50,sy=50,miny=-30000,maxy=-200,spawn_at={"default:stone"},
+			on_spawn=function(pos)
+				local s = 75
+				local pos1 = vector.subtract(pos,s)
+				local pos2 = vector.add(pos,s)
+				local air = minetest.get_content_id("air")
+				local lava = minetest.get_content_id("default:lava_source")
+				local bedrock = minetest.get_content_id("default:bedrock")
+				local water = minetest.get_content_id("default:salt_water_source")
+				local vm = minetest.get_voxel_manip()
+				local min, max = vm:read_from_map(pos1, pos2)
+				local area = VoxelArea:new({MinEdge = min, MaxEdge = max})
+				local data = vm:get_data()
+				for z=-s,s do
+				for y=-s,s do
+				for x=-s,s do
+					local id=area:index(pos.x+x,pos.y+y,pos.z+z)
+					local d1 = vector.length(vector.new({x=x,y=y,z=z}))/s
+					local d2 = vector.length(vector.new({x=x,y=0,z=z}))/s
+
+					if y <=-1 and (math.abs(x) <= 24 and math.abs(z) <= 24) then
+						data[id] = bedrock
+					elseif y <= -s/1.5 and d2 < 1.1 then
+						data[id] = lava
+					elseif d1 < 1 or y <= 0 and d2 < 1 then
+						data[id] = air
+					elseif d1 >= 1 and data[id] == water then
+						data[id] = bedrock
+					end
+					id=id+1
+				end
+				end	
+				end
+				vm:set_data(data)
+				vm:write_to_map()
+
+				local r = {"0","90","180","270"}
+				local pos3
+				r = r[math.random(1,4)]
+
+				minetest.place_schematic(pos,minetest.get_modpath("places").."/schematics/places_lavacastle.mts",r,nil,true,"place_center_x,place_center_z")
+
+				if r == "0" then
+					pos3 = vector.new(pos.x,pos.y+1,pos.z-50)
+				elseif r == "180" then
+					pos3 = vector.new(pos.x,pos.y+1,pos.z+50)
+				elseif r == "90" then
+					pos3 = vector.new(pos.x-50,pos.y+1,pos.z)
+				elseif r == "270" then
+					pos3 = vector.new(pos.x+50,pos.y+1,pos.z)
+				end
+				minetest.place_schematic(pos3,minetest.get_modpath("places").."/schematics/places_lavacastle_bridge.mts",r,nil,false,"place_center_x,place_center_z")
+				local nodes = minetest.find_nodes_in_area(vector.subtract(pos,30),vector.add(pos,30),"materials:iron_chest")
+				for i,v in pairs(nodes) do
+					default.treasure({level=2,pos=v,node="materials:iron_chest"})
+				end
+			end
+		},
 		["stairs to sky"]={
-			chance=50,sx=10,sy=250,miny=0,maxy=50,spawn_at={"default:dirt"},
+			chance=50,sx=10,sy=250,miny=-20,maxy=70,spawn_at={"default:dirt"},
 			on_spawn=function(pos)
 				local p2
 				for y=0,5 do
@@ -102,7 +161,7 @@ minetest.register_on_generated(function(minp, maxp, seed)
 	local area
 	local data
 	for i,v in pairs(places.buildings) do
-		if math.random(math.random(0,v.chance),v.chance) == 0 and minp.y-100 <= v.miny and maxp.y+100 >= v.maxy then
+		if math.random(0,v.chance) == 0 and (v.miny <= minp.y and v.maxy >= maxp.y) then
 			local spawn_at = {}
 			for i1,v1 in pairs(v.spawn_at) do
 				spawn_at[minetest.get_content_id(v1)] = true
@@ -115,7 +174,7 @@ minetest.register_on_generated(function(minp, maxp, seed)
 			for t=0,10 do
 				local tx = vector.new(math.random(minp.x,maxp.x),maxp.y,math.random(minp.z,maxp.z))
 				for y=maxp.y,minp.y,-1 do
-					if y >= v.miny and y <= v.maxy and spawn_at[data[area:index(tx.x,y,tx.z)]] then
+					if spawn_at[data[area:index(tx.x,y,tx.z)]] then
 						local sp = vector.new(tx.x,y,tx.z)
 						minetest.emerge_area(vector.subtract(sp,v.sx,v.sy,v.sx),vector.add(sp,v.sx,v.sy,v.sx))
 						minetest.after(0.01,function(v,sp)
@@ -134,6 +193,6 @@ minetest.register_tool("places:spawn", {
 	inventory_image = "default_stick.png",
 	on_use=function(itemstack, user, pointed_thing)
 		local pos = user:get_pos()
-		places.buildings["stairs to sky"].on_spawn(vector.round(pos))
+		places.buildings["lava castle"].on_spawn(vector.round(pos))
 	end
 })
