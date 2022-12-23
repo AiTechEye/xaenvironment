9c7bc40bba1925a5a993198dc9459150551ae36b
AiTechEye <40591179+AiTechEye@users.noreply.github.com>
2022-12-23 17:53:50 +0100

    Add files via upload

--- mods/examobs/mob_types.lua
+++ mods/examobs/mob_types.lua
@@ -366,11 +366,11 @@ examobs.register_roadwalker=function(def)
 
 	nodedef.groups = nodedef.groups or {on_load=1,attached_node=1,not_in_creative_inventory=1}
 	nodedef.drawtype = nodedef.drawtype or "airlike"
-	nodedef.sunlight_propagates = nodedef.sunlight_propagates ~= false
+	nodedef.sunlight_propagates = nodedef.sunlight_propagates or true
 	nodedef.drop = nodedef.drop or ""
-	nodedef.walkable = nodedef.walkable ~= false
+	nodedef.walkable = nodedef.walkable or false
 	nodedef.pointable = nodedef.pointable or false
-	nodedef.floodable = nodedef.floodable ~= false
+	nodedef.paramtype = nodedef.paramtype or "light"
 
 	nodedef.on_load = function(pos)
 		examobs.paths[mobname] = examobs.paths[mobname] or {}
