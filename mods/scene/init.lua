c5dcfbf67d20cde45b0eb26ddbd9fe3d60867347
AiTechEye <40591179+AiTechEye@users.noreply.github.com>
2024-01-22 16:27:38 +0100

    Delete mods/scene/init.lua

--- mods/scene/init.lua
+++
@@ -1,8 +0,0 @@
-scene = {
-	user = {},
-	animtime = 0,
-	animjobs = {},
-	scenes = {},
-}
-dofile(minetest.get_modpath("scene") .. "/api.lua")
-dofile(minetest.get_modpath("scene") .. "/scenes.lua")
